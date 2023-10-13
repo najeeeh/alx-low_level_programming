11221ed79fff0c33de62240785f94d0d391cbe67
najeeeh <rahali.najeeh@gmail.com>
2023-10-13 09:39:12 -0700

    function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,22 +1,32 @@
-#ifndef _HEADER_
-#define _HEADER_
+#include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * struct op - Struct op
- * @op: The operator
- * @f: The function associated
+ * get_op_func - selects the correct function to perform
+ * the operation asked by the user
+ * @s: operator passed as argument to the program
+ * Return: pointer to the function that corresponds to
+ * the operator given as a parameter andif s does not match
+ * any of the 5 expected operators, return NULL
  */
-typedef struct op
+int (*get_op_func(char *s))(int, int)
 {
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
+	op_t ops[] = {
+		{ "+", op_add },
+		{ "-", op_sub },
+		{ "*", op_mul },
+		{ "/", op_div },
+		{ "%", op_mod },
+		{ NULL, NULL }
+	};
+	int i = 0;
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
-
-#endif
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (0);
+}
