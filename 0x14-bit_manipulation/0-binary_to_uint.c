5fb7fb50392222a317103fd5c88c6e1bad3c8f32
najeeeh <rahali.najeeh@gmail.com>
2023-10-26 07:15:16 -0700

    converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		deci = 2 * decim + (b[i] - '0');
+		decim = 2 * decim + (b[i] - '0');
 	}
 	return (decim);
 }
