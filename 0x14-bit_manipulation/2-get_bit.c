c9518c04492381b9aca584f528dd34e0386ddca1
najeeeh <rahali.najeeh@gmail.com>
2023-10-26 08:09:51 -0700

    tasks

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_bit - a function that returns the value of a bit at a given index.
+ * @n: decimal number to look into its binary version
+ * @index: index of the bit
+ * Return: value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_val;
+
+	if (index > 63)
+		return (-1);
+	bit_val = (n >> index) & 1;
+	return (bit_val);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,4 +2,9 @@
 #define MAIN_H
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 #endif
